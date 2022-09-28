684cedac5ede4f6af331ec2a08173fab555ca2de
sixway <lw.2675@qq.com>
2022-09-28 21:58:10 +0800

    feat: parse feilu to fetch book info

---
+++ feilu.py
@@ -0,0 +1,23 @@
+# 飞卢小说数据抓取
+
+import re
+
+import requests
+
+
+def pages():
+    yield from (f"https://b.faloo.com/y_0_{i + 1}.html" for i in range(10))
+
+
+pattern = re.compile(
+    r'<h1 class="fontSize17andHei" title=".*? target="_blank" '
+    r'title="(?P<name>.*?)">.*?</a></h1>.*?ontSize14andHui"><a.*?html" '
+    r'title="(?P<type>.*?)" target="_blank".*?<span>'
+    r'周点击：(?P<click>.*?)</span>.*?字数：(?P<count>.*?)</span>.*?'
+    r'日期：<font class="fontSize14andChen"><font color="#ff6600">(?P<date>.*?)</font>')
+for page in pages():
+    r = requests.get(page)
+    text = r.content.decode('gbk')
+    result = pattern.finditer(text)
+    for i in result:
+        print(i.groupdict())
